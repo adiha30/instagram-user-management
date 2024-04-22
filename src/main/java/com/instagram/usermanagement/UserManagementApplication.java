46a381fd51f1b54560bb942f1f9098948e2d709f
Adi Harel <adi.harel@nice.com>
2024-04-22 23:36:54 +0300

    optimized imports

--- src/main/java/com/instagram/usermanagement/UserManagementApplication.java
+++ src/main/java/com/instagram/usermanagement/UserManagementApplication.java
@@ -1,6 +1,5 @@
 package com.instagram.usermanagement;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
--- src/main/java/com/instagram/usermanagement/services/UserService.java
+++ src/main/java/com/instagram/usermanagement/services/UserService.java
@@ -3,8 +3,6 @@ package com.instagram.usermanagement.services;
 import com.instagram.usermanagement.models.User;
 import com.instagram.usermanagement.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
